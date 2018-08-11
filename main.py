4a7dca6eb95b9390877b834d519737c15ca0b159
Andrew Seaman <andrewseaman@Andrews-MacBook-Pro.local>
2018-08-10 17:01:31 -0700

    Added a genre chooser to templates and main.py + few minor brushups

--- main.py
+++ main.py
@@ -19,8 +19,14 @@ class Profile(webapp2.RequestHandler):
         template=jinja_env.get_template('/templates/profile.html')
         self.response.write(template.render())
 
+class GenreChooser(webapp2.RequestHandler):
+    def get(self):
+        template=jinja_env.get_template('/templates/genre_chooser.html')
+        self.response.write(template.render())
+
 #the app configuration section
 app = webapp2.WSGIApplication([
     ('/', MainPage),
-    ('/profile', Profile)#this maps the root url to the MainPage Handler
+    ('/profile', Profile),
+    ('/genre-chooser', GenreChooser)#this maps the root url to the MainPage Handler
 ], debug=True)
