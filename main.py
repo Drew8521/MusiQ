53ba992917d5fa777e900ab9513db619d4981092
Andrew Seaman <andrew.seaman@mullenmail.com>
2018-08-14 10:43:42 -0700

    Got the highscore list to work and added name customization on profile

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ import time
 from google.appengine.api import users
 from google.appengine.ext import ndb
 from musiq_models import User
+from profile_methods import create_profile, logout_url, login_url
 
 jinja_env = jinja2.Environment(
     loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),
@@ -15,8 +16,6 @@ jinja_env = jinja2.Environment(
 #the handler section
 class LoginHandler(webapp2.RequestHandler):
     def get(self):
-        logout_url = users.create_logout_url('/')
-        login_url = users.create_login_url('/')
         new_user_template = jinja_env.get_template("templates/new_user.html")
         google_login_template = jinja_env.get_template("templates/google_login.html")
         # get Google user
@@ -45,7 +44,10 @@ class LoginHandler(webapp2.RequestHandler):
 class Profile(webapp2.RequestHandler):
     def get(self):
         template=jinja_env.get_template('/templates/profile.html')
-        self.response.write(template.render({"sign_out": users.create_logout_url('/')}))
+        user = users.get_current_user()
+        current_user = User.query().filter(User.email == user.email()).get()
+        profile_fields = create_profile(current_user)
+        self.response.write(template.render(profile_fields))
 
     def post(self):
         user = users.get_current_user()
@@ -76,9 +78,47 @@ class DifficultyChooser(webapp2.RequestHandler):
         template=jinja_env.get_template('/templates/difficulty_chooser.html')
         self.response.write(template.render())
 
+class SeedHandler(webapp2.RequestHandler):
+    def get(self):
+        new_user_entry = User(
+        user_name = "Arthur",
+        user_nickname = "Giggle",
+        email = "asdf@asdf.com",
+        highscore = 12
+        )
+        new_user_entry.put()
+        new_user_entry = User(
+        user_name = "Dalman",
+        user_nickname = "DaMan",
+        email = "asd@asdf.com",
+        highscore = 19
+        )
+        new_user_entry.put()
+        new_user_entry = User(
+        user_name = "Sal",
+        user_nickname = "NotSal",
+        email = "IActuallyAmSal@asdf.com",
+        highscore = 42
+        )
+        new_user_entry.put()
+        new_user_entry = User(
+        user_name = "SerialKiller",
+        user_nickname = "DefNotAKiller",
+        email = "Puppies@asdf.com",
+        highscore = 39
+        )
+        new_user_entry.put()
+        new_user_entry = User(
+        user_name = "JohnCena",
+        user_nickname = "CenaYouCenaMe",
+        email = "YouCantSeeMe@asdf.com",
+        highscore = 104
+        )
+        new_user_entry.put()
 #the app configuration section
 app = webapp2.WSGIApplication([
     ('/', LoginHandler),
+    ('/seed', SeedHandler),
     ('/profile', Profile),
     ('/genre-chooser', GenreChooser),
     ('/difficulty-chooser', DifficultyChooser)#this maps the root url to the MainPage Handler
