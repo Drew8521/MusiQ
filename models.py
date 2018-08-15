f1901536a2a79976f53b445e1c8521f6293cf1ac
Isaiah <anyiminene@gmail.com>
2018-08-15 12:32:05 -0700

    added data to datastore

--- models.py
+++ models.py
@@ -4,7 +4,7 @@ from google.appengine.ext import ndb
 
 class Song(ndb.Model):
     genre = ndb.StringProperty(required=True)
-    title = ndb.StringProperty(required=True)
+    song = ndb.StringProperty(required=True)
     artist = ndb.StringProperty(required=True)
     album = ndb.StringProperty(required=True)
-    link = ndb.StringProperty(required=True)
+    url = ndb.StringProperty(required=True)
